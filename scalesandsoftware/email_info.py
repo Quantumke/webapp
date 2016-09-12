a9bac22535acf2f3c5ff3670fa2ee6ebbe143d6a
quantumke <nguruben@gmail.com>
2016-09-12 07:17:33 +0300

    email info module + WSGI module

---
+++ scalesandsoftware/email_info.py
@@ -0,0 +1,6 @@
+#--------------------------------------------------------------------------------------------------------------------python send email()
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'nguruben@gmail.com'
+EMAIL_HOST_PASSWORD = 'wambuinguru'
